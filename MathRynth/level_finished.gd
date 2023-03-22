0f2bea2dbf2191a9c025deda6a8ca8170759fc68
LAPTOP-VSSU1O4B\Riina <riinamuld@gmail.com>
2023-03-22 08:56:25 +0200

    lisasin loetavuse parandamiseks uue fondi Nunito-SemiBold ja tegin mängus vastavad muudatused

--- MathRynth/level_finished.gd
+++ MathRynth/level_finished.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,4 @@ func _ready():
 
 
 func _on_to_main_menu_pressed():
-    Transition.change_scene("res://main.tscn")
+	Transition.change_scene("res://main.tscn")
